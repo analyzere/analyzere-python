0f9797027d4b9db91c7f48df55a28c173102f56c
Daniel Arndt <dan@arndt.ca>
2016-03-01 14:36:26 -0400

    Fix flake8 test failure

--- analyzere/base_resources.py
+++ analyzere/base_resources.py
@@ -126,8 +126,8 @@ class AnalyzeReObject(object):
         return repr_str
 
     def __eq__(self, other):
-        return (isinstance(other, self.__class__)
-                and self.__dict__ == other.__dict__)
+        return (isinstance(other, self.__class__) and
+                self.__dict__ == other.__dict__)
 
     def __ne__(self, other):
         return not self.__eq__(other)
