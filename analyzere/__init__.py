7a04eb428d12e8c950b72858e76bd62ac0310134
Duane Wilson <duane@analyzere.com>
2019-04-17 09:14:06 -0300

    v0.6.0 release
    - Feature: Added `currencies` method to `ExchangeRateTable` class, allowing users to retrieve a list of currency codes.
    - Feature: Added `candidate_parameters` method to `OptimizationView` class, allowing users to retrieve only the
    parameters for a given optimization candidate.
    - Feature: Added `candidate_metrics` method to `OptimizationView` class, allowing users to retrieve only the
    metrics (objectives and constraints) for a given optimization candidate.
    - Feature: Added `sensitivity_analysis` method to `OptimizationView` class, allowing users to retrieve the
    senstivity analysis output for a selected set of candidates.

--- analyzere/__init__.py
+++ analyzere/__init__.py
@@ -5,7 +5,7 @@ upload_poll_interval = 0.1
 one_megabyte = 2**20
 upload_chunk_size = 16 * one_megabyte
 tls_verify = True
-user_agent = 'analyzere-python 0.5.5-dev'
+user_agent = 'analyzere-python 0.6.0-dev'
 
 from analyzere.resources import (  # noqa
     AnalysisProfile,
