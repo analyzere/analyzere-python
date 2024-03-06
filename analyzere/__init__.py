bd3f39223b4ef178d711d1d71948ff27fd518041
aravindg28 <aravind.govindarajan@analyzere.com>
2024-03-06 16:42:55 -0400

    Update Python and flake8 version

--- analyzere/__init__.py
+++ analyzere/__init__.py
@@ -5,7 +5,7 @@ upload_poll_interval = 0.1
 one_megabyte = 2**20
 upload_chunk_size = 16 * one_megabyte
 tls_verify = True
-user_agent = 'analyzere-python 0.7.0-dev'
+user_agent = 'analyzere-python 0.7.1-dev'
 
 from analyzere.resources import (  # noqa
     AnalysisProfile,
